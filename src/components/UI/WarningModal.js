028db93f72b6e349b2136b4705ec533924119859
ekil <idgusqls0506@naver.com>
2022-07-05 00:35:51 +0900

    경고창 만드는 중

--- src/components/UI/WarningModal.js
+++ src/components/UI/WarningModal.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import Button from './Button';
 import styles from './WarningModal.module.css';
 
-const WarningModal = props => {};
+const WarningModal = props => {
+  const okButtonClickHandler = () => {
+
+  };
+
+  return (
+    <section>
+      <h1>{props.warningMessage}</h1>
+      <p>{props.children}</p>
+      <Button type="button" onClick={okButtonClickHandler}>Okay</Button>
+    </section>
+  )
+};
 
 export default WarningModal;
--- src/components/Users/UserInput.js
+++ src/components/Users/UserInput.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import Button from '../UI/Button'
+import WarningModal from '../UI/WarningModal';
 import styles from './UserInput.module.css';
 
 const UserInput = props => {
@@ -9,19 +10,38 @@ const UserInput = props => {
     userAge: ''
   });
 
+  const [hasValue, setHasValue] = useState(true);
+
   const [isValid, setIsValid] = useState(true);
 
   const userNameInputChangeHandler = e => {
+    if (e.target.value.trim().length > 0) {
+      setHasValue(true);
+    }
     setEnteredValue({userName: e.target.value, userAge: ''}); // 이름 저장
   };
 
   const userAgeInputChangeHandler = e => {
+    if (e.target.value.trim().length > 0) {
+      setHasValue(true);
+    }
+    if (e.target.value >= '0') {
+      setIsValid(true);
+    }
     (enteredValue.userName !== '') ? setEnteredValue({userName: enteredValue.userName, userAge: e.target.value}) : console.log('냠')
   }; // 이름 없으면 경고창 띄울까?
 
   const formSubmitHandler = e => {
     e.preventDefault();
-    
+    if (enteredValue.userName.trim().length === 0 || enteredValue.userAge.trim().length === 0) {
+      setHasValue(false);
+      return;
+    }
+    if (enteredValue.userAge < 0) {
+      setIsValid(false);
+      setEnteredValue({userName:'', userAge:''});
+      return;
+    }
     props.onAddUser(enteredValue);
     console.log(enteredValue);
     setEnteredValue({userName:'', userAge:''}) // 제출 시 입력창 초기화, 아래 value에 이거 전달
@@ -38,6 +58,12 @@ const UserInput = props => {
         <input type="number" onChange={userAgeInputChangeHandler} value={enteredValue.userAge} />
       </div>
       <Button type="submit">Add User</Button>
+      <WarningModal warningMessage='Invalid input' className="hasValue">
+        Please enter a valid name and age &#40;non-empty values&#41;.
+      </WarningModal>
+      <WarningModal warningMessage='Invalid input' className="isValid">
+        Please enter a valid age &#40;&gt; 0&#41;.
+      </WarningModal>
     </form>
   )
 };
